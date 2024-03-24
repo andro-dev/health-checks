42cb4bf333061411924462d94bcdeb368c01932a
andrew <androdev.cft6@gmail.com>
2024-03-23 21:45:16 -0400

    Check free GB first before calculating persent_free in check_disk_util

--- all_checks.py
+++ all_checks.py
@@ -25,7 +25,7 @@ def check_disk_full(disk, min_gb, min_percent):
 def main():
     if check_reboot():
         print("Pending Reboot.")
-        sys,exit(1)
+        sys.exit(1)
     if check_disk_full(disk="/", min_gb=2, min_percent=10):
         print("Disk full.")
         sys.exit(1)
